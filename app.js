1d1ca5fd6d78b64368aa30307f1e8e52ba041055
Damien Dormal <damien.dormal@mokosocialmedia.com>
2016-01-04 12:52:47 +0800

    Move to ES6

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-require('dotenv').load();
-var express = require('express');
-var fs = require('fs');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+'use strict';
 
-var models = path.join(__dirname, 'models');
+require('dotenv').load();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const models = path.join(__dirname, 'models');
 
 // Bootstrap models
 fs.readdirSync(models)
   .filter(file => ~file.indexOf('.js'))
   .forEach(file => require(path.join(models, file)));
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
 mongoose.connect(process.env.DB);
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,7 +40,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -49,7 +51,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -60,7 +62,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
+'use strict';
+
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const Article = mongoose.model('Article');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   Article.find({}, (err, articles) => {
     if (err) return res.render('500');
     res.render('index', {
